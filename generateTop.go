fe0d12f60a7fb00c4515c6a5e6f80c04308a5a08
maseology <maseology@pm.me>
2020-04-24 08:12:12 -0400

    added generateTop

---
+++ generateTop.go
@@ -0,0 +1,61 @@
+package montecarlo
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/maseology/montecarlo/sampler"
+)
+
+const maxtrials = 10
+
+// GenerateTop returns nsamples of function evaluations that exceed the minOF
+func GenerateTop(fp string, eval func(u []float64) float64, s sampler.Set, nsamples int, minOF float64) {
+	tim := time.Now()
+	cnt, iter := 0, 0
+	coll := make([][]float64, 0, nsamples*maxtrials)
+	for {
+		uFinal, rFinal := GenerateSamples(eval, s.Ndim, nsamples)
+		for i, f := range rFinal {
+			if f > minOF {
+				lst := make([]float64, s.Ndim+1)
+				lst[0] = f
+				for ii, v := range uFinal[i] {
+					lst[ii+1] = v
+				}
+				coll = append(coll, lst)
+				cnt++
+			}
+		}
+		if cnt >= nsamples {
+			fmt.Printf("\n  %d samples in %d iterations -- %v\n", cnt, iter, time.Now().Sub(tim))
+			saveGob(fp, s, coll)
+			break
+		}
+		iter++
+		if iter == maxtrials && cnt == 0 {
+			fmt.Printf("\n  no sample found to meet minimum objective -- %v\n", time.Now().Sub(tim))
+			break
+		}
+	}
+}
+
+// saveGob can be read using mcpig
+func saveGob(fp string, s sampler.Set, coll [][]float64) {
+	f, err := os.Create(fp)
+	defer f.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	enc := gob.NewEncoder(f)
+	err = enc.Encode(s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = enc.Encode(coll)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ mcpig/gob.go
@@ -0,0 +1,90 @@
+package mcpig
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"math"
+	"os"
+
+	"github.com/maseology/mmio"
+	"github.com/maseology/montecarlo/sampler"
+)
+
+const nbins = 30
+
+// ReadMCPIG reads a single sampling
+func ReadMCPIG(gobfp string) {
+	wcsv, _ := mmio.NewTXTwriter(mmio.RemoveExtension(gobfp) + ".csv")
+	defer wcsv.Close()
+	wcsv.WriteLine("station,bin,par,val")
+	writeToCsv(wcsv, gobfp)
+}
+
+// ReadMCPIGs reads a set of samplings (i.e., from multiple stations)
+func ReadMCPIGs(mcdir string) {
+	wcsv, _ := mmio.NewTXTwriter(mcdir + "summary.csv")
+	defer wcsv.Close()
+	wcsv.WriteLine("station,bin,par,val")
+	for _, fp := range mmio.FileListExt(mcdir, ".gob") {
+		fmt.Println(fp)
+		writeToCsv(wcsv, fp)
+	}
+}
+
+func readGob(fp string) (sampler.Set, [][]float64, error) {
+	var d [][]float64
+	var s sampler.Set
+	f, err := os.Open(fp)
+	defer f.Close()
+	if err != nil {
+		return sampler.Set{}, nil, err
+	}
+	enc := gob.NewDecoder(f)
+	err = enc.Decode(&s)
+	if err != nil {
+		return sampler.Set{}, nil, err
+	}
+	err = enc.Decode(&d)
+	if err != nil {
+		return sampler.Set{}, nil, err
+	}
+	return s, d, nil
+}
+
+func writeToCsv(w *mmio.TXTwriter, gobfp string) {
+	denom := 0.
+	ss, coll, err := readGob(gobfp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// intialize bins
+	bins := make([][]float64, ss.Ndim)
+	for i := 0; i < ss.Ndim; i++ {
+		bins[i] = make([]float64, nbins)
+		for j := 0; j < nbins; j++ {
+			bins[i][j] = 0.
+		}
+	}
+
+	for _, v := range coll {
+		objfnc := v[0]
+		for i := 0; i < ss.Ndim; i++ {
+			ii := int(math.Floor(v[i+1] * float64(nbins)))
+			bins[i][ii] += objfnc
+		}
+		denom++
+	}
+
+	denom /= float64(nbins)
+	par, gnam := ss.ParameterNames(), mmio.FileName(gobfp, false)
+	for j := 0; j < nbins; j++ {
+		for i := 0; i < ss.Ndim; i++ {
+			score := bins[i][j] / denom
+			val := ss.Samplers[i].Sample((float64(j) + .5) / float64(nbins))
+			// w.WriteLine(fmt.Sprintf("%s,%d,%s,%f", gnam, j+1, par[i], score))
+			w.WriteLine(fmt.Sprintf("%s,%f,%s,%f", gnam, val, par[i], score))
+		}
+	}
+}
